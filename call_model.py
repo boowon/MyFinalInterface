624da735e8269e5a106580e594dae863f89397ba
boowon <94376439+boowon@users.noreply.github.com>
2023-05-19 14:46:39 +0100

    Update

--- call_model.py
+++ call_model.py
@@ -9,12 +9,11 @@ import tensorflow_text as tf_text
 def cnn_model(path):
     img = cv2.imread(path, cv2.IMREAD_GRAYSCALE) # load an image
     blur = cv2.medianBlur(img, 5) # apply median filter
-    cv2.imwrite(path, blur) #Orignal
-    image = tf.io.read_file(path)
-    image = tf.image.decode_image(image,1)#creat image into pixels
-    image = tf.image.convert_image_dtype(image, dtype=tf.float32) #normalize x/255
-    image = tf.image.resize(image, [428,480]) # (60,60,1)
-    image = tf.expand_dims(image, axis=0) # (1,60,60,1)
+    image = tf.image.convert_image_dtype(blur, dtype=tf.float32) #normalize x/255
+    image = tf.expand_dims(image, axis=2)  # (x,y,1)
+    image = tf.image.resize(image, [428,480])
+    image = tf.expand_dims(image, axis=0) # (1,x,y,1)
+    
     model = load_model(r'C:\Users\bougu\Documents\GitHub\MyFinalInterface\Model', compile=False)
     
     dict_maladie = {0: 'Rien a signaler', 1: 'Diabtic', 2: 'Occlusion', 3: 'Oedeme papillaire', 4: 'Myopie', 5: 'Maculophatie'}
@@ -26,7 +25,6 @@ def cnn_model(path):
     maladie = dict_maladie[pred]
     return maladie
     
-
 
 def cnn_test(path):
     image = tf.io.read_file(path)
