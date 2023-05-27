2aee30c3d76fce844d341433ea3df30c08b0316d
boowon <94376439+boowon@users.noreply.github.com>
2023-05-27 20:29:51 +0100

    Update Angeo.py

--- Angeo.py
+++ Angeo.py
@@ -13,17 +13,17 @@ def Pred(path):
     maladieName = cnn_model(path)
     print(maladieName)
     messagebox.showinfo(title="Maladie", message=maladieName)
-    
+'''  
 def confexit():
     yesno = messagebox.askyesno(title="Exit", message="Are sure you want to exit?") #return 1 fro yes and 0 for No
     if yesno == 1:
         root.quit()
-
+'''  
 def submit():
     #create a db or connect to it
     cnx = sqlite3.connect("basededonne.db")
     cur = cnx.cursor()
-    #cur.execute('''DROP TABLE IF EXISTS Data_Base_pateients''')
+    #cur.execute(DROP TABLE IF EXISTS Data_Base_pateients)
     #creat a table
     cur.execute(""" CREATE TABLE IF NOT EXISTS Data_Base_pateients (
               First_name text,
@@ -32,19 +32,19 @@ def submit():
                 )
                 """)
     #inesrt infos
-    if fname.get()!="":
-        if lname.get()!="":
+    if first_name.get()!="":
+        if last_name.get()!="":
             cur.execute("INSERT INTO Data_Base_pateients VALUES (:First_name, :Last_name, :Gender)",
-                 { 'First_name' : fname.get(),
-                   'Last_name' : lname.get(),
+                 { 'First_name' : first_name.get(),
+                   'Last_name' : last_name.get(),
                    'Gender': gender.get()
                  }
                 ) 
             cnx.commit()
             cnx.close()
 
-            fname.delete(0, END)
-            lname.delete(0, END)
+            first_name.delete(0, END)
+            last_name.delete(0, END)
         else :
             messagebox.showerror(title="ERROR", message="The Last name field is empty")
     else:
@@ -53,127 +53,194 @@ def submit():
 def diaglog():
     global imgradio, lbl, path
     photo = filedialog.askopenfile(initialdir="C:/Users/bougu/Downloads", title="Select the Photo", filetypes=(("jpg files","*.jpg"),("png files","*.png"), ("All files","*.*")))
-    addbtn.destroy() 
-    textlbl.destroy() 
-    analybtn['state'] = NORMAL
-    dltphbtn['state'] = NORMAL
+    add_btn.destroy() 
+    text_lbl.destroy() 
+    analy_btn['state'] = NORMAL
+    dltph_btn['state'] = NORMAL
     path = str(photo)
     path = path.split("'")[1]
     image = Image.open(path)
     image = image.resize((400, 400), Image.LANCZOS)
     imgradio = ImageTk.PhotoImage(image)
-    lbl = Label(framephoto, image=imgradio)
+    lbl = Label(frame_photo, image=imgradio)
     lbl.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 def redraw_selcet():
     btnfont2 = font.Font(family='Courier', weight='bold', size=13)
     lbl.destroy()
-    analybtn['state'] = DISABLED
-    dltphbtn['state'] = DISABLED
-    textlbl = Label(framephoto, text="Select Photo", font=btnfont2)
-    addbtn = Button(framephoto,text="+", borderwidth=0, font=btnfont2, command=diaglog)
+    analy_btn['state'] = DISABLED
+    dltph_btn['state'] = DISABLED
+    textlbl = Label(frame_photo, text="Select Photo", font=btnfont2)
+    addbtn = Button(frame_photo,text="+", borderwidth=0, font=btnfont2, command=diaglog)
     textlbl.place(relx=0.5, rely=0.4, anchor=CENTER)
     addbtn.place(relx=0.5, rely=0.5, anchor=CENTER)
     
-
 
 
 def forminfo():
-    global fname, lname, age, gender
-    btnfont3 = font.Font(family='Courier', weight='bold', size=10)
+    global first_name, last_name, age, gender
+    from_font = font.Font(family='Courier', weight='bold', size=10)
+    
     #inputs             
-    fname = Entry(frameinfo, width=20)
-    lname = Entry(frameinfo, width=20)
-    age = DateEntry(frameinfo, selectmode='day')
-    genderfr = LabelFrame(frameinfo, text="Gender", pady=3, background="#96765a", font=btnfont3)
+    first_name = Entry(frame_info, width=20)
+    last_name = Entry(frame_info, width=20)
+    age = DateEntry(frame_info, selectmode='day')
+    gender_frame = LabelFrame(frame_info, text="Gender", pady=3, background="#96765a", font=from_font)
     gender = StringVar()
     gender.set("Female")
-    Radiobutton(genderfr, text="Female", variable=gender, value="Female", background="#96765a", font=btnfont3).pack(side=LEFT)
-    Radiobutton(genderfr, text="Male", variable=gender, value="Male", background="#96765a", font=btnfont3).pack(side=LEFT)
+    Radiobutton(gender_frame, text="Female", variable=gender, value="Female", background="#96765a", font=from_font).pack(side=LEFT)
+    Radiobutton(gender_frame, text="Male", variable=gender, value="Male", background="#96765a", font=from_font).pack(side=LEFT)
     
-    fname.grid(row= 0, column=1, pady=10)
-    lname.grid(row= 1, column=1, pady=10)
+    first_name.grid(row= 0, column=1, pady=10)
+    last_name.grid(row= 1, column=1, pady=10)
     age.grid(row= 2, column=1, pady=10)
-    genderfr.grid(row=3, column=0, columnspan=2,sticky=EW, pady=10)
+    gender_frame.grid(row=3, column=0, columnspan=2,sticky=EW, pady=10)
     
     #titles
-    fnamelbl = Label(frameinfo, text= "First Name", anchor=W, background="#96765a", font=btnfont3)
-    lnamelbl = Label(frameinfo, text= "Last Name", anchor=W, background="#96765a", font=btnfont3)
-    agelbl = Label(frameinfo, text= "Birthdate", anchor=W, background="#96765a", font=btnfont3)
-    fnamelbl.grid(row= 0, column=0, padx=5, pady=10, sticky=W+E)
-    lnamelbl.grid(row= 1, column=0, padx=5, pady=10,sticky=W+E)
-    agelbl.grid(row= 2, column=0, sticky=W+E)
+    first_name_lbl = Label(frame_info, text= "First Name", anchor=W, background="#96765a", font=from_font)
+    last_name_lbl = Label(frame_info, text= "Last Name", anchor=W, background="#96765a", font=from_font)
+    age_lbl = Label(frame_info, text= "Birthdate", anchor=W, background="#96765a", font=from_font)
+    first_name_lbl.grid(row= 0, column=0, padx=5, pady=10, sticky=W+E)
+    last_name_lbl.grid(row= 1, column=0, padx=5, pady=10,sticky=W+E)
+    age_lbl.grid(row= 2, column=0, sticky=W+E)
+    
     #buttoon
     btnfont = font.Font(family='Courier', weight='bold', size=10)
-    btn = Button(frameinfo, text="Submit", command=submit, background="White", font=btnfont, anchor=S)
+    #, command=submit
+    btn = Button(frame_info, text="Submit", background="White", font=btnfont, anchor=S)
     btn.grid(row= 4, column=0, columnspan=2, sticky=N+S)
-              
+
+            
+            
+#Frame for New Patient  
 def pass_new_patient():
-    imglabel.destroy()
-    btnnewpatient.destroy()
-    btnoldpatient.destroy()
+    
+    #destroy the old frame
+    imgLabel.destroy()
+    btnNewPatient.destroy()
+    btnOldPatient.destroy()
+    
+    #create a new frame
     root.configure(background='white')
-    global frameinfo, addbtn, textlbl, analybtn, framephoto,dltphbtn 
-    #top bar
-    framesup = LabelFrame(root, background="#96765a", borderwidth=0)
-    framesup.grid(row=0, column=0, columnspan=2, sticky=EW)  
-    btnfont1 = font.Font(family='Courier', weight='bold', size=8)
-    btnhelp = Button(framesup, text="Help", borderwidth=0, background="#96765a", font=btnfont1, padx=2)
-    btnhelp.grid(row=0, column=0)
-    root.grid_columnconfigure(0, weight=1)
-    btnexit = Button(framesup, text="Exit", borderwidth=0, background="#96765a", font=btnfont1, command=confexit)
-    btnexit.grid(row=0, column=1)
+    global frame_info, add_btn, text_lbl, analy_btn, frame_photo, dltph_btn, frame_tool, frame_analy, frame_analy_in
+    
+    #create a frame for the tools bar
+    frame_tool = LabelFrame(root, background="#96765a", borderwidth=0)
+    frame_tool.grid(row=0, column=0, columnspan=2, sticky=EW)  
+    btn_font1 = font.Font(family='Courier', weight='bold', size=8)
+    
+    #Help button
+    btn_help = Button(frame_tool, text="Help", borderwidth=0, background="#96765a", font=btn_font1, padx=2)
+    btn_help.grid(row=0, column=0)
+    
+    #Exit button
+    #, command=confexit
+    btn_exit = Button(frame_tool, text="Exit", borderwidth=0, background="#96765a", font=btn_font1)
+    btn_exit.grid(row=0, column=1)
     
     #Fill your patient info
-    btnfont2 = font.Font(family='Courier', weight='bold', size=13)
-    frameinfo = LabelFrame(root, text="Entry patient informations:", background="#96765a", padx= 50, font=btnfont2)
-    frameinfo.grid(row=1, column=0, sticky=NSEW) 
-    root.grid_rowconfigure(1, weight=1)
-    root.grid_columnconfigure(0, weight=0)  
+    frame_font = font.Font(family='Courier', weight='bold', size=13)
+    frame_info = LabelFrame(root, text="Entry patient informations:", background="#96765a", padx= 50, font=frame_font)
+    frame_info.grid(row=1, column=0, sticky=NSEW) 
+    
+    #Form info function
     forminfo()
+ 
+    #Part AI 
+    
+    #Frame for the photo
+    
+    frame_analy = LabelFrame(root, background="white")
+    frame_analy_in = LabelFrame(frame_analy, text="Analysis:", padx= 50, pady=50, font=frame_font, background="white")
+    frame_photo = LabelFrame(frame_analy_in, bd="1", relief=SUNKEN)
+    text_lbl = Label(frame_photo, text="Select Photo", font=frame_font)
+    #
+    add_btn = Button(frame_photo,text="+", borderwidth=0, font=frame_font, command=diaglog)
+    text_lbl.place(relx=0.5, rely=0.4, anchor="center")
+    add_btn.place(relx=0.5, rely=0.55, anchor="center")
+    frame_photo.grid(row=0, column=0, sticky=NSEW)
+    
+    frame_analy_in.grid(row=0, column=0, rowspan=2, sticky=NSEW, padx=15, pady=15)
+    frame_analy_in.grid_rowconfigure(0, weight=1) 
+    frame_analy_in.grid_columnconfigure(0, weight=1)
+    
+
+    #Analyse button
+    #
+    analy_btn = Button(frame_analy_in, text="Start analyse", font=btn_font1, state=DISABLED, command=lambda: Pred(path))
+    analy_btn.grid(row=1, column=0, pady=30, ipadx=60)
 
     
-    #Part AI 
-    frameanaly = LabelFrame(root, text="Analysis:", padx= 50, pady=50, font=btnfont2, background="white")
-    framephoto = LabelFrame(frameanaly, bd="1", relief=SUNKEN)
-    textlbl = Label(framephoto, text="Select Photo", font=btnfont2)
-    addbtn = Button(framephoto,text="+", borderwidth=0, font=btnfont2, command=diaglog)
-    textlbl.place(relx=0.5, rely=0.4, anchor=CENTER)
-    addbtn.place(relx=0.5, rely=0.5, anchor=CENTER)
-    framephoto.grid(row=0, column=0,columnspan=2, sticky=NSEW) 
-    frameanaly.grid_rowconfigure(0, weight=1)
-    frameanaly.grid_columnconfigure(0, weight=1) 
+    #Delete photo button
+    #
+    dltph_btn = Button(frame_analy, text="Delete photo", font=btn_font1, state=DISABLED, command=redraw_selcet)
+    dltph_btn.grid(row=2, column=0, pady=20, padx=10, sticky=W+S)
     
-    analybtn = Button(frameanaly, text="Start analyse", font=btnfont1, state=DISABLED, command=lambda: Pred(path))
-    analybtn.grid(row=1, column=0, pady=20)
+    #Return Home button
+    return_btn = Button(frame_analy, text="Return Home", font=btn_font1, command=return_home)
+    return_btn.grid(row=2, column=1, pady=20, sticky=S)
     
-    dltphbtn = Button(frameanaly, text="Delete photo", font=btnfont1, state=DISABLED, command=redraw_selcet)
-    dltphbtn.grid(row=1, column=1, pady=20)
+    frame_analy.grid(row=1, column=1, sticky=NSEW) 
     
-    frameanaly.grid(row=1, column=1, sticky=NSEW, padx=15, pady=15) 
+    frame_analy.grid_rowconfigure(0, weight=1) 
+    frame_analy.grid_columnconfigure(0, weight=1)
+    
     root.grid_rowconfigure(1, weight=1) 
     root.grid_columnconfigure(1, weight=1) 
 
-
-
-
+#Return Home function
+def return_home():
     
-def expand():
-    global imglabel,i,btnnewpatient,btnoldpatient
-    if i <len(imglist):
-        imglabel.destroy()
-        imglabel = Label(root, image=imglist[i] ,borderwidth=0)
-        imglabel.place(relx=0.5, rely=0.5, anchor=CENTER)
+    #Return to initial state
+    frame_info.destroy()
+    frame_photo.destroy()
+    frame_tool.destroy()
+    frame_analy.destroy()
+    root.configure(background='#96765a')
+    
+    global imgLabel,i,btnNewPatient,btnOldPatient
+    i=0
+    
+    if i <len(imgList):
+        imgLabel.destroy()
+        imgLabel = Label(root, image=imgList[i] ,borderwidth=0)
+        imgLabel.place(relx=0.5, rely=0.5, anchor=CENTER)
         i+=1
         root.after(75,expand)
     else:
-        btnfont = font.Font(family='Courier', weight='bold', size=8)
-        btnnewpatient = Button(root, text="New patient", background="White", width=55, height=3, font=btnfont, command=pass_new_patient)
-        btnnewpatient.place(relx=0.25, rely=0.9, anchor=CENTER)
-        btnoldpatient = Button(root, text="Old patient", background="white", width=55, height=3, font=btnfont)
-        btnoldpatient.place(relx=0.75, rely=0.9, anchor=CENTER)  
+        btn_font = font.Font(family='Courier', weight='bold', size=8)
+        
+        #New Patient button
+        btnNewPatient = Button(root, text="New patient", background="White", width=55, height=3, font=btn_font, command=pass_new_patient)
+        btnNewPatient.place(relx=0.25, rely=0.9, anchor=CENTER)
+        
+        #Old Patient button
+        btnOldPatient = Button(root, text="Old patient", background="white", width=55, height=3, font=btn_font)
+        btnOldPatient.place(relx=0.75, rely=0.9, anchor=CENTER)  
+
+
+#First frame: Home   
+def expand():
+    global imgLabel,i,btnNewPatient,btnOldPatient
+    if i <len(imgList):
+        imgLabel.destroy()
+        imgLabel = Label(root, image=imgList[i] ,borderwidth=0)
+        imgLabel.place(relx=0.5, rely=0.5, anchor=CENTER)
+        i+=1
+        root.after(75,expand)
+    else:
+        btn_font = font.Font(family='Courier', weight='bold', size=8)
+        
+        #New Patient button
+        btnNewPatient = Button(root, text="New patient", background="White", width=55, height=3, font=btn_font, command=pass_new_patient)
+        btnNewPatient.place(relx=0.25, rely=0.9, anchor=CENTER)
+        
+        #Old Patient button
+        btnOldPatient = Button(root, text="Old patient", background="white", width=55, height=3, font=btn_font)
+        btnOldPatient.place(relx=0.75, rely=0.9, anchor=CENTER)  
 
         
+#Create window, title, icon, size        
 root = Tk()
 root.title("Ophthalmology Angiography")
 root.configure(background='#96765a')
@@ -262,10 +329,10 @@ image = Image.open("C:/Users/bougu/Documents/GitHub/MyFinalInterface/icon.png")
 image = image.resize((500, 500), Image.LANCZOS)
 img101 = ImageTk.PhotoImage(image)
 
-imglist = [img, img1, img2, img21, img3, img31, img4, img41, img5, img51, img6, img61, img7, img71, img8, img81, img9, img91, img10, img101]
+imgList = [img, img1, img2, img21, img3, img31, img4, img41, img5, img51, img6, img61, img7, img71, img8, img81, img9, img91, img10, img101]
 
-imglabel = Label(root, image=imglist[0] ,borderwidth=0, anchor=CENTER)
-imglabel.place(relx=0.5, rely=0.5, anchor=CENTER)
+imgLabel = Label(root, image=imgList[0] ,borderwidth=0, anchor=CENTER)
+imgLabel.place(relx=0.5, rely=0.5, anchor=CENTER)
 i=1
 expand()
 
